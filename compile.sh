7f953da2c8608153f3e5813662027e0882c3f087
gongchen322 <chengong@pal-nat184-077-104.itap.purdue.edu>
2015-03-27 19:57:58 -0400

    Update
    set up the working environment and make the java file be able to run

---
+++ compile.sh
@@ -0,0 +1,2 @@
+export PATH=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home:$PATH
+javac Crawler.java
---
+++ run.sh
@@ -0,0 +1,2 @@
+export PATH=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home:$PATH
+java -cp "./mysql-connector-java-5.1.15-bin.jar:." Crawler
